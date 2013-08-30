079cf04901dba14f18a20356f6d51b35e3cde647
Giovanni Lovato <heruan@aldu.net>
2013-08-30 16:57:43 +0200

    Merged new tokenfield with typeahead support

--- www/js/TicketViewModel.js
+++ www/js/TicketViewModel.js
@@ -4,20 +4,55 @@ function Customer(customer) {
 
 function User(contact) {
     this.cn = ko.observable(contact.cn);
+    this.sAMAccountName = ko.observable(contact.sAMAccountName);
     this.mail = ko.observableArray(contact.mail);
+}
+
+function Note(note) {
+    this.author = ko.observable(new User(note.author));
+    this.created = ko.observable(new Date(note.created));
+    this.public = ko.observable(note.public);
+    this.via = ko.observable(note.via);
+    this.viaString = ko.computed(function() {
+        switch (this.via().media) {
+            case 'web':
+                return this.via().media;
+            case 'phonecall':
+                return [
+                    this.via().media,
+                    'answered by',
+                    this.via().by.cn
+                ].join(' ');
+        }
+    }, this);
+    this.text = ko.observable(note.text);
+    this.attachments = ko.observableArray(note.attachments);
+    this.previousStatus = ko.observable(note.previousStatus);
+    this.currentStatus = ko.observable(note.currentStatus);
+    this.previousAssignee = ko.observable(note.previousAssignee && new User(note.previousAssignee));
+    this.currentAssignee = ko.observable(note.currentAssignee && new User(note.currentAssignee));
 }
 
 function Ticket(ticket) {
     this.number = ko.observable();
     this.status = ko.observable();
+    this.queue = ko.observable();
+    this.type = ko.observable();
     this.severity = ko.observable();
     this.customer = ko.observable();
     this.contact = ko.observable();
+    this.assignee = ko.observable();
     this.responseTime = ko.observable();
     this.interventionTime = ko.observable();
     this.resolutionTime = ko.observable();
     this.tags = ko.observableArray();
     this.notes = ko.observableArray();
+    this.cc = ko.observableArray();
+    this.ccList = ko.computed(function() {
+        return $.map(this.cc(), function(cc) {
+            return cc.cn();
+        }).join(',');
+    }, this);
 
     this.timeString = function(time) {
         if (time < 60) {
@@ -79,14 +114,22 @@ function Ticket(ticket) {
     this.update = function(ticket) {
         this.number(ticket.number);
         this.status(ticket.status);
+        this.queue(ticket.queue);
+        this.type(ticket.type);
         this.severity(ticket.severity);
         this.customer(new Customer(ticket.customer));
         this.contact(new User(ticket.contact));
+        this.assignee(new User(ticket.assignee));
         this.responseTime(ticket.responseTime);
         this.interventionTime(ticket.interventionTime);
         this.resolutionTime(ticket.resolutionTime);
         this.tags(ticket.tags);
-        this.notes(ticket.notes);
+        this.notes($.map(ticket.notes, function(note) {
+            return new Note(note);
+        }));
+        this.cc($.map(ticket.cc, function(user) {
+            return new User(user);
+        }));
 
         switch (ticket.status) {
             case 0:
@@ -115,15 +158,26 @@ function Ticket(ticket) {
 
 var lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ultrices ligula. Phasellus vestibulum fringilla ipsum nec imperdiet. Duis id accumsan erat. Vestibulum neque purus, faucibus id sagittis ut, dictum quis purus. Phasellus non nibh mollis, ullamcorper elit eu, dignissim lorem. Mauris tincidunt leo non lorem ornare, eu vehicula lacus viverra.';
 
+var _aro = {
+    cn : 'Giovanni Lovato',
+    sAMAccountName : 'LovatoG'
+};
+
 var _ticket = {
     number : '201308270001',
-    status : 0,
+    status : 2,
+    queue : 2,
+    type : 0,
     severity : 0,
     customer : {
         name : 'Fiera di Vicenza'
     },
     contact : {
         cn : 'Sergio Casagrande',
+    },
+    assignee : {
+        cn : 'Lorenzo Di Pace',
+        sAMAccountName : 'DiPaceL'
     },
     responseTime : 465,
     interventionTime : 3600,
@@ -136,7 +190,12 @@ var _ticket = {
             },
             created : '2013-08-27T17:28',
             public : true,
-            via : 'web',
+            via : {
+                media : 'phonecall',
+                by : {
+                    cn : 'Luca Marchioron'
+                }
+            },
             text : lipsum,
             attachments : [],
             previousStatus : null,
@@ -150,7 +209,12 @@ var _ticket = {
             },
             created : '2013-08-27T17:36',
             public : true,
-            via : 'web',
+            via : {
+                media : 'web',
+                by : {
+                    cn : 'Luca Marchioron'
+                }
+            },
             text : '',
             attachments : [],
             previousStatus : 0,
@@ -166,7 +230,12 @@ var _ticket = {
             },
             created : '2013-08-27T18:36',
             public : false,
-            via : 'web',
+            via : {
+                media : 'web',
+                by : {
+                    cn : 'Giovanni Lovato'
+                }
+            },
             text : lipsum,
             attachments : [
                 {
@@ -190,12 +259,19 @@ var _ticket = {
             }
         }
 
+    ],
+    cc : [
+        {
+            cn : 'Alessandro Sudiro'
+        }
     ]
 };
 
 var _ticket2 = {
     number : '201308270002',
     status : 1,
+    queue : 1,
+    type : 1,
     severity : 2,
     customer : {
         name : 'Finiper'
@@ -207,3 +283,5 @@ var _ticket2 = {
     interventionTime : 4365,
     resolutionTime: 215
 };
+
+
